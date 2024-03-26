4cb0d54ab15138f7f5a5ec541a79945e25592b03
Anusha Salman <anushasalman@Anushas-MacBook-Pro.local>
2024-03-25 21:13:12 -0500

    second commit, puppies showing in array on console

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-
-
+  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppyList);
   return (
       <div>
-
+{   
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
       </div>
   )
 
