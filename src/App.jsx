9125c722e0f736fdd6531aa507a799dd08272a54
Anusha Salman <anushasalman@Anushas-MacBook-Pro.local>
2024-03-25 21:19:40 -0500

    third commit, adding id to give unique key props

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import './App.css'
 
 function App() {
   
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
   return (
-      <div>
+      <div className="App">
 {   
    puppies.map((puppy) => {
-     return <p>{puppy.name}</p>
+     return <p key={puppy.id}>{puppy.name}</p>;
    })
 }
       </div>
-  )
+  );
 
 }
 export default App
