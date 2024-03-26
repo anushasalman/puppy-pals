b2ba57260dc65072b443a2304768b4f4264a0990
Anusha Salman <anushasalman@Anushas-MacBook-Pro.local>
2024-03-25 21:48:49 -0500

    trying to render details of each puppy

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,36 @@ import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log();
 
-  console.log("puppyList: ", puppyList);
+  function handleClick() {
+    // some logic here
+  }
 
   return (
-      <div className="App">
-{   
-   puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
-   })
-}
-      </div>
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      <p>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      </p>
+    </div>
   );
-
 }
 export default App
